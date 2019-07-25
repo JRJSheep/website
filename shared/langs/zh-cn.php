3af46bb9317261573475c96668a1c8a418c25d5b
JRJSheep <37053560+JRJSheep@users.noreply.github.com>
2019-07-25 09:10:52 +0800

    Update zh-cn.php
    Updated Chinese Simplified text and adapted to new interface tips

--- shared/langs/zh-cn.php
+++ shared/langs/zh-cn.php
@@ -36,6 +36,9 @@ $s['file'] = '文件';
 $s['expires'] = '过期日期';
 $s['sha1'] = 'SHA-1';
 $s['size'] = '大小';
+$s['additionalEdition'] = '附加版本';
+$s['requiredEdition'] = '所需版本';
+$s['unknown'] = '未知';
 
 //global
 $s['home'] = '首页';
@@ -60,8 +63,8 @@ $s['newlyAdded'] = '新增的内部版本';
 $s['dateAdded'] = '添加日期';
 
 //known.php
-$s['browseKnown'] = '浏览已知补丁';
-$s['chooseBuild'] = '选择补丁';
+$s['browseKnown'] = '浏览已知内部版本';
+$s['chooseBuild'] = '选择内部版本';
 $s['weFoundBuilds'] = '在你的查询中，我们找到了 <b>%d</b> 个内部版本。'; //We have found <b>692</b> builds for your query.
 
 //latest.php
@@ -81,7 +84,7 @@ $s['fetchUpdatesInfo'] = '点击 <i>获取更新</i> 按钮将你的请求发送
 $s['responseFromServer'] = '来自服务器的响应';
 $s['foundUpdates'] = '找到 %d 个更新'; //Found 1 update(s)
 $s['foundTheseUpdates'] = '找到了以下更新，点击所需更新的名称以继续。';
-$s['buildNumber'] = '版本号： %s'; //Build number: 18890.1000
+$s['buildNumber'] = '内部版本号： %s'; //Build number: 18890.1000
 
 //selectlang.php
 $s['selectLangFor'] = '选择 %s 的语言'; //Select language for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64
@@ -96,6 +99,11 @@ $s['selectLangInfoText1'] = '点击 <i>下一步</i> 按钮以选择你要下载
 $s['selectLangInfoText2'] = 'WindowsUpdateBox.exe 和累积更新可以在 <i>全语言</i> 中找到。';
 $s['allLangsWarn'] = '<i>全语言</i> 选项不支持版本选择。';
 $s['clickNextToOpenFindFiles'] = '点击 <i>下一步</i> 按钮打开页面，该页面允许查找文件。';
+$s['noLangsAvailable'] = '此内部版本没有可用的语言。';
+$s['browseFiles'] = '浏览文件';
+$s['browseFilesDesc'] = '快速浏览所选内部版本中的文件';
+$s['searchFiles'] = '搜索文件';
+$s['toSearchForCUUseQuery'] = '若要搜索累积更新，请使用 <i>%s</i> 搜索查询。'; //To search for Cumulative Updates use the <i>Windows10 KB</i> search query.
 
 //selectedition.php
 $s['selectEditionFor'] = '选择 %s 的版本'; //Select edition for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64, English (United States)
@@ -103,6 +111,7 @@ $s['chooseEdition'] = '选择版本';
 $s['chooseEditionDesc'] = '选择你想要的版本';
 $s['allEditions'] = '全版本';
 $s['selectEditionInfoText'] = '点击 <i>下一步</i> 按钮以打开所选内容的版本摘要页面。';
+$s['additionalEditionsInfo'] = '如果你有需要，可在右侧表格中找到其他版本，请选择指定的所需版本，然后单击<i>下一步</i>。在摘要页面上，你可以在适当的下载选项下选择所需的其他版本。';
 
 //download.php
 $s['summary'] = '摘要';
@@ -134,7 +143,7 @@ $s['browseUpdatesList'] = '浏览更新列表';
 $s['listOfFilesFor'] = '%s 的文件列表'; //List of files for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64
 $s['totalSizeOfFiles'] = '文件总大小：%s'; //Total size of files: 2.86 GiB
 $s['fileRenamingScript'] = '文件重命名脚本';
-$s['fileRenamingScriptDesc1'] = '下面的脚本可用于快速重命名下载的文件。';
+$s['fileRenamingScriptDesc1'] = '下方的脚本可用于快速重命名下载的文件。';
 $s['fileRenamingScriptDesc2'] = '只需将下面表单的内容复制到带有 <code>cmd</code> 扩展名的新文件，并将其放在带有下载文件的文件夹中运行即可。';
 $s['sha1File'] = 'SHA-1 校验和文件';
 $s['sha1FileDesc'] = '你可以使用此文件快速验证文件是否已正确下载。';
@@ -165,20 +174,20 @@ $s['arm64Warning'] = '你选择的 ARM64 版本 <b>仅与基于 ARM64 的设备�
 $s['error_ERROR'] = '一般错误。';
 $s['error_UNSUPPORTED_API'] = '已安装的 API 版本与此版本的 UUP dump 不兼容。';
 $s['error_NO_FILEINFO_DIR'] = '<i>fileinfo</i> 目录不存在。';
-$s['error_NO_BUILDS_IN_FILEINFO'] = '<i>fileinfo</i> 数据库不包含任何补丁。';
-$s['error_SEARCH_NO_RESULTS'] = '找不到执行查询的项目。';
+$s['error_NO_BUILDS_IN_FILEINFO'] = '<i>fileinfo</i> 数据库不包含任何内部版本。';
+$s['error_SEARCH_NO_RESULTS'] = '无法找到可执行查询的项目。';
 $s['error_UNKNOWN_ARCH'] = '未知的处理器体系结构。';
 $s['error_UNKNOWN_RING'] = '未知的环。';
 $s['error_UNKNOWN_FLIGHT'] = '未知的通道。';
 $s['error_UNKNOWN_COMBINATION'] = '通道和环的组合不正确。 只有快速环才支持向前跳过。';
 $s['error_ILLEGAL_BUILD'] = '指定的内部版本号小于 %d 或大于 %d。'; //Specified build number is less than 9841 or larger than 2147483646.
-$s['error_ILLEGAL_MINOR'] = '指定的次要/服务版本不正确。';
+$s['error_ILLEGAL_MINOR'] = '指定的修订内部版本不正确。';
 $s['error_NO_UPDATE_FOUND'] = '服务器没有返回任何更新。';
 $s['error_XML_PARSE_ERROR'] = '响应 XML 解析失败。 Microsoft 服务器可能存在问题，请稍后再试。';
 $s['error_EMPTY_FILELIST'] = '服务器返回了一个空的文件列表。';
 $s['error_NO_FILES'] = '没有可供您选择的文件。';
-$s['error_NOT_FOUND'] = '找不到指定的选择。';
-$s['error_MISSING_FILES'] = '选定的 UUP 集缺少文件。';
+$s['error_NOT_FOUND'] = '无法找到指定的选择。';
+$s['error_MISSING_FILES'] = '指定的 UUP 集缺少文件。';
 $s['error_NO_METADATA_ESD'] = '没有可供您选择的 ESD 元数据文件。';
 $s['error_UNSUPPORTED_LANG'] = '不支持指定的语言。';
 $s['error_UNSPECIFIED_LANG'] = '没有指定语言。';
